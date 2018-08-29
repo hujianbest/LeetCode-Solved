88bfa15d0699fd00c82dd534c62a82e3c2539e50
unknown <jianbesthu@163.com>
2018-08-29 13:33:25 +0800

    store more message to save time

---
+++ src/Min Stack.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ */
+
+class MinStack {
+
+    Stack<Integer> stack;//保存元素
+    Stack<Integer> minStack;//保存最小值
+    /** initialize your data structure here. */
+    public MinStack() {
+        this.stack = new Stack<>();
+        this.minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty() || x<=minStack.peek()) minStack.push(x);
+    }
+    
+    public void pop() {
+        int p = stack.pop();
+        if(p==minStack.peek()) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
